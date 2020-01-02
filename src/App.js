9693e47cae348b9039131c974876f83aaec92672
Derek jacoby <derekja@gmail.com>
2020-01-01 16:01:31 -0800

    initial port to new create-react-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {component} from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Play from './play.js'
+import Stories from './stories.js'
+import Author from './author.js'
+import Intro from './intro.js'
+import logo from './images/vicstories.svg';
 import './App.css';
 
 function App() {
+
+
   return (
+    <Router>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Link to={'/'} className="nav-link">
+          <img src={logo} className="App-logo" alt="logo" />
+        </Link>
+        <h2>  Welcome to Victoria Stories</h2>
+        <nav className="navbar navbar-light bg-light">
+          <ul className="navbar-nav mr-auto App-link">
+            <li><Link to={'/stories'} className="nav-link">Play Stories</Link></li>
+            <li><Link to={'/author'} className="nav-link">Author your own</Link></li>
+          </ul>
+        </nav>
       </header>
+      <Switch>
+        <Route exact path='/' component={Intro} />
+        <Route exact path='/play' component={Play} />
+        <Route path='/stories' component={Stories} />
+        <Route path='/author' component={Author} />
+      </Switch>
     </div>
+
+    </Router>
   );
 }
 
